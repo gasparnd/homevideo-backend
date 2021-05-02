9002397302a369ecfc3b84727fa48306bf8b7ebf
Gaspar Dolcemascolo <gaspardolcemascolo@gmail.com>
2021-05-02 17:19:06 -0300

    add cache system

--- routes/movies.js
+++ routes/movies.js
@@ -10,6 +10,11 @@ const {
 
 const validationHandler = require('../utils/middleware/validationHandler')
 
+const cacheResponse = require('../utils/cacheResponse')
+const { 
+    FIVE_MINUTES_IN_SECONDS,
+    SIXTY_MINUTES_IN_SECONDS } = require('../utils/time')
+
 function moviesApi(app) {
     const router = express.Router();
     app.use('/api/movies', router);
@@ -17,6 +22,7 @@ function moviesApi(app) {
     const moviesService = new MoviesService();
 
     router.get('/', async (req, res, next) => {
+        cacheResponse(res, FIVE_MINUTES_IN_SECONDS)
         const { tags } = req.query;
         try {
             const movies = await moviesService.getMovies({ tags });
@@ -31,6 +37,7 @@ function moviesApi(app) {
     });
 
     router.get('/:movieId', validationHandler(joi.object({ movieId: movieIdSchema }), 'params'), async (req, res, next) => {
+        cacheResponse(res, SIXTY_MINUTES_IN_SECONDS)
         const { movieId } = req.params;
         try {
             const movies = await moviesService.getMovie({ movieId })
